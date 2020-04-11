a8d2b7b2f7cbd3218fb2a4dda8d692bd60a27a49
paboustead <46978518+paboustead@users.noreply.github.com>
2020-04-10 17:49:11 -0700

    Add files

---
+++ auth.js
@@ -0,0 +1,74 @@
+const express = require('express');
+const router = express.Router();
+const https = require('https');
+
+consumerKey='****INSERT_YOUR_KEY_HERE****';
+consumerSecret='****INSERT_YOUR_SECRET_HERE****';
+
+router.post('/token',(req, res, next) => {
+    const credentials = new Buffer.from(consumerKey+':'+consumerSecret).toString('base64');
+    
+    const data = JSON.stringify({
+        'grant_type': 'client_credentials',
+    });
+    const options = {
+        hostname: 'session.voxeet.com',
+        port: 443,
+        path: '/v1/oauth2/token',
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Cache-Control': 'no-cache',
+            'Authorization': 'Basic ' + credentials
+        }
+    };
+    var token="";
+    var httpreq = https.request(options, function (response) {
+        response.setEncoding('utf8');
+        response.on('data', function (chunk) {
+            token=token+chunk;
+        });
+        response.on('end', function() {
+          res.contentType='application/json;charset=UTF-8';
+          res.status(200).json(JSON.parse(token));
+        })
+      });
+      httpreq.write(data);
+      httpreq.end();
+    
+})
+
+router.post('/refresh',(req, res, next) => {
+    const credentials = new Buffer.from('YTRtMDZyanI1cHI2Nw=='+':'+'M3NtajhtZ2RzMzk3NmtoanNmNDlwY2lsY2w=').toString('base64');
+    
+    const data = JSON.stringify({
+        "refresh_token": "string",
+    });
+    const options = {
+        hostname: 'session.voxeet.com',
+        port: 443,
+        path: '/v1/oauth2/refresh',
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Cache-Control': 'no-cache',
+            'Authorization': 'Basic ' + credentials
+        }
+    };
+    var token="";
+    var httpreq = https.request(options, function (response) {
+        response.setEncoding('utf8');
+        response.on('data', function (chunk) {
+            token=token+chunk;
+        });
+        response.on('end', function() {
+          res.contentType='application/json;charset=UTF-8';
+          res.status(200).json(JSON.parse(token));
+        })
+      });
+      httpreq.write(data);
+      httpreq.end();
+    
+})
+
+module.exports = router;
