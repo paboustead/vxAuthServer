cd65c6e9119b0e6d7918eda6408384fa68cced58
paboustead <46978518+paboustead@users.noreply.github.com>
2020-04-13 17:53:00 -0700

    Update server.js

--- server.js
+++ server.js
@@ -4,9 +4,10 @@ const app = require('./app');
 const port = process.env.port || 3000;
 const fs = require('fs')
 
+// Comment out line below to setup HTTPS
 const server = http.createServer(app);
 
-// For HTTPS token server
+// For HTTPS token server uncomment lines below and add path to private key and certificate files
 /* const server = https.createServer({
   key: fs.readFileSync('REPLACE WITH PATH TO privkey.pem'),
   cert: fs.readFileSync('REPLACE WITH PATH TO fullchain.pem')
